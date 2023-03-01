150b9bda37744dd870c2eb9fe33c0774de0db00a
NateLapak <Nate.lapak12@gmail.com>
2023-02-28 21:57:52 -0700

    Made directory for new pages

---
+++ frontend/src/Components/Play/Playpage.jsx
@@ -0,0 +1,9 @@
+const Playpage = () => {
+    return (
+        <div>
+            <h2>Queue to play a game</h2>
+        </div>
+    )
+}
+
+export default Playpage
---
+++ frontend/src/Pages/Play.jsx
@@ -0,0 +1,11 @@
+import Playpage from "../Components/Play/Playpage";
+
+const Play = () => {
+    return (
+        <div>
+            <Playpage />
+        </div>
+    )
+}
+
+export default Play
